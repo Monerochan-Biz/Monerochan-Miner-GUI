2622551082d6f31c024d832526407f01825c2f2e
Crypt0-Bear <22245297+Crypt0-Bear@users.noreply.github.com>
2022-04-25 22:09:08 -0500

    new structure for file

--- miner.py
+++ miner.py
@@ -5,10 +5,15 @@ from monerochan_gui import *
 
 config = configparser.ConfigParser()
 # config.read_file(open('mine.cfg'))
+config['DEFAULT'] = {'seed': '45',
+                    'Compression': 'yes',
+                      'CompressionLevel': '9'}
 
-
-
+def loadWallet():
+    wallet = Wallet(config)
+    print(wallet.SEEDPHRASE)
 
 if __name__ == '__main__':
     gui = MonerochanGUI(config)
+    loadWallet()
     input("Close?")
--- monerochan_gui.py
+++ monerochan_gui.py
@@ -4,6 +4,12 @@ import time
 import random
 import configparser
 import json
+import monero
+
+# from monero.backends.offline import OfflineWallet, WalletIsOffline
+
+# Running xmrig with tor socks proxy 
+# command = f"./xmrig -x 127.0.0.1:9050 -a rx/0 -o {onionHost}:{onionPort} -u {mineAddress}"
 
 
 class Settings:
@@ -12,6 +18,23 @@ class Settings:
         self.title = "Monerochan Miner GUI"
 
 
+class Wallet:
+    def __init__(self,_config):
+        self.SEEDPHRASE = self.loadOrCreate(_config)
+        #This is the seed class
+        self.seed = monero.seed.Seed(self.SEEDPHRASE)
+
+    def getPublicAddress(self):
+        return self.seed.public_address()
+
+
+
+    def loadOrCreate(self,_config):
+        if _config is not None:
+            seed = monero.seed.Seed().phrase
+        else:
+            seed = _config["seed"]
+        return seed
 
 
 
